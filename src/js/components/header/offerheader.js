48f8d5f9cb77665969e12095652afe6f9f6b1ba9
Michael Clelland <michael.clelland@mender.io>
2021-02-03 15:22:21 +0100

    Updated offer banner show/hide logic and text Changelog: None
    Signed-off-by: Michael Clelland <michael.clelland@mender.io>

--- src/js/components/header/offerheader.js
+++ src/js/components/header/offerheader.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import { LocalOffer as LocalOfferIcon, Close as CloseIcon } from '@material-ui/icons';
 
-const OfferHeader = () => (
+const OfferHeader = ({ organization, onHide }) => (
   <div id="offerHeader" className="offerBox">
     <LocalOfferIcon style={{ marginRight: '2px', height: '16px', verticalAlign: 'bottom' }} />
-    <span>
-      If you are on a paid plan you can enable <b>Remote terminal</b> for free with your current subscription until March 31st.{' '}
-      <a href="https://docs.mender.io/add-ons/remote-terminal" target="_blank" rel="noreferrer">
-        Read the documentation to learn how to enable it
-      </a>
-    </span>
+    {organization.trial ? (
+      <span>
+        Try out the new <b>Remote terminal</b> feature for free with your current trial.&nbsp;
+      </span>
+    ) : (
+      <span>
+        Try out the new <b>Remote terminal</b> feature for free on your current plan until March 31st.&nbsp;
+      </span>
+    )}
+    <a href="https://docs.mender.io/add-ons/remote-terminal" target="_blank" rel="noreferrer">
+      Read the documentation to learn how to enable it
+    </a>
+    <CloseIcon style={{ marginLeft: '4px', height: '16px', cursor: 'pointer' }} onClick={onHide} />
   </div>
 );
 

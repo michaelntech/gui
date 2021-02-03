9c01d816711cd13d0546511206e7add19fb663a8
Michael Clelland <michael.clelland@mender.io>
2021-02-03 11:11:59 +0100

    Add rel attribute to link Changelog: None
    Signed-off-by: Michael Clelland <michael.clelland@mender.io>

--- src/js/components/header/offerheader.js
+++ src/js/components/header/offerheader.js
@@ -6,7 +6,7 @@ const OfferHeader = () => (
     <LocalOfferIcon style={{ marginRight: '2px', height: '16px', verticalAlign: 'bottom' }} />
     <span>
       If you are on a paid plan you can enable <b>Remote terminal</b> for free with your current subscription until March 31st.{' '}
-      <a href="https://docs.mender.io/add-ons/remote-terminal" target="_blank">
+      <a href="https://docs.mender.io/add-ons/remote-terminal" target="_blank" rel="noreferrer">
         Read the documentation to learn how to enable it
       </a>
     </span>
